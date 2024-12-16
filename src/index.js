6b3eb3082f4e0d4ae92bac021fc8ba8fea854e8b
Mark Miller <mark.o.miller@gmail.com>
2024-12-16 11:21:55 -0700

    admin route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { prettyJSON } from 'hono/pretty-json'
-// import { basicAuth } from 'hono/basic-auth'
+import { basicAuth } from 'hono/basic-auth'
 // import { HTTPException } from 'hono/http-exception'
 
 const app = new Hono()
@@ -10,6 +10,20 @@ app.use('*', cors())
 
 app.get('/', (c) => c.redirect('https://acmg.ca'))
 
+// Basic AuthN
+app.use('/admin/*', async (c, next) => {
+    const auth = basicAuth({
+        username: 'wicketstaging',
+        password: c.env.WICKET_STAGING_BASICAUTHPASSWORD,
+        realm: 'WICKET_STAGING_TOKEN'
+    })
+    return auth(c, next)
+})
+
 app.get('/hello', (c) => c.text('hello world'))
 
+app.get('/admin', () => {
+
+    c.text('secure')
+})
 export default app
