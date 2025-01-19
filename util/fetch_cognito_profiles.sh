b6ccddd63b58153cc6115a0ae4367794f4c98572
Mark Miller <mark.o.miller@gmail.com>
2025-01-19 11:53:03 -0700

    data refresh

--- util/fetch_cognito_profiles.sh
+++ util/fetch_cognito_profiles.sh
@@ -7,7 +7,19 @@
 #----
 
 # Use the Cognito Forms API to fetch all the member profiles
-for id in {1..3197}; do
-    echo $id
-    curl -s -S -f -o ./profile_data/${id}.json "https://www.cognitoforms.com/api/forms/267/entries/$id?access_token=$cognito_api_key"
+# was up to 3179
+for id in {725..3230}; do
+    
+    while true; do
+        status_code=$(curl -s -S -f -o ./profile_data/${id}.json -w "%{http_code}" "https://www.cognitoforms.com/api/forms/267/entries/$id?access_token=$cognito_api_key")
+
+        if [[ "$status_code" == "429" || "$status_code" == "500" ]]; then
+            echo "$id >> $status_code"
+            echo "Backing off... Sleep for 5 minutes."
+            sleep 300
+        else
+            echo "$id >> $status_code"
+            break
+        fi
+    done
 done
