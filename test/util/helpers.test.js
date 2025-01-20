9d4e0ad111bda0a5023e8286800ec870b18c58be
Mark Miller <mark.o.miller@gmail.com>
2025-01-20 13:25:50 -0700

    profile cleaner

--- test/util/helpers.test.js
+++ test/util/helpers.test.js
@@ -1,6 +1,6 @@
-import { addFortyFourMonths } from "../../util/helpers.js";
+import { addFortyFourMonths, cleanCognitoMyProfile } from "../../util/helpers.js";
 
-import { expect, test } from "bun:test";
+import { describe, expect, test } from "bun:test";
 
 test('Can add 44 months to a date', () => {
 
@@ -19,4 +19,16 @@ test('Throws a type error if a Date is not provided when adding 44 months', () =
     }
     expect(t).toThrow(TypeError)
     expect(t).toThrow('Input must be a Date to add 44 months to it.')
+})
+
+describe('Cognito profile simplification', () => {
+
+    test('produces a simple formatted string with the expected data', async () => {
+
+        const profileFile = Bun.file(`./profile_data/974.json`);
+        const profile = await profileFile.json();
+        const cleanedProfile = cleanCognitoMyProfile(profile)
+        expect(cleanedProfile.split('\n')).toHaveLength(94)
+    })
+
 })
--- util/helpers.js
+++ util/helpers.js
@@ -71,3 +71,75 @@ export const addFortyFourMonths = (date) => {
 
     return addMonths(date, 44)
 }
+
+/**
+ * Convert a Cognito My Profile data object into a simpler string representation of the same.
+ * @param {Object} p - Profile object representing the original state from Cognito Forms
+ * @returns {String} A stripped down, formatted, string representation of the original profile suitable for storage in Wicket MDP
+ */
+export const cleanCognitoMyProfile = (p) => {
+
+    delete p.Form
+    delete p.MemberPhoto
+    delete p.IFMGAPadLeft
+    delete p.Entry.User
+    delete p.Entry.Action
+    delete p.Entry.Role
+    delete p.Entry.Order
+    delete p.Entry.PublicLink
+    delete p.Entry.InternalLink
+    delete p.Entry.Number
+    delete p.Entry.Document1
+    delete p.Entry.Document2
+    delete p.Entry.Document3
+    delete p.Entry.Document4
+    Object.keys(p).forEach((k) => {
+
+        if(!p[k]) delete p[k]
+        if(k.endsWith('_QuantitySelected')) delete p[k]
+        if(k.endsWith('_QuantityUsed')) delete p[k]
+        if(k.endsWith('_QuantityLimit')) delete p[k]
+        if(k.endsWith('_QuantityLimitCalculated')) delete p[k]
+        if(k.endsWith('_IncrementBy')) delete p[k]
+        if(k.endsWith('_Maximum')) delete p[k]
+        if(Array.isArray(p[k]) && p[k].length === 0) delete p[k]
+    })
+    if(!p.Nationality1.Id) delete p.Nationality1
+    if(!p.Nationality2.Id) delete p.Nationality2
+    if(p.AdministrativeNotes && p.AdministrativeNotes.length > 0){
+
+        p.AdministrativeNotes.forEach((note) => {
+
+            delete note.Id 
+            delete note.ItemNumber
+        })
+    }
+    Object.keys(p.LegalName).forEach((k) => {
+
+        if(!p.LegalName[k]) delete p.LegalName[k]
+    })
+    Object.keys(p.Residence).forEach((k) => {
+
+        if(!p.Residence[k]) delete p.Residence[k]
+        delete p.Residence.FullInternationalAddress
+    })
+    Object.keys(p.Mailing).forEach((k) => {
+
+        if(!p.Mailing[k]) delete p.Mailing[k]
+        delete p.Mailing.FullInternationalAddress
+    })
+    // If mailing is the same as residence, delete mailing
+    const keepMailing = Object.keys(p.Residence).map((k) => {
+
+        return p.Residence[k] !== p.Mailing[k]
+    }).reduce((acc, cur) => {
+
+        if(cur === true) acc = true
+        return acc
+    }, false)
+    if(!keepMailing) delete p.Mailing
+    p.FirstAidProviderLabel = p.FirstAidProvider.Label 
+    delete p.FirstAidProvider
+    delete p.Id
+    return JSON.stringify(p, null, 2)
+}
